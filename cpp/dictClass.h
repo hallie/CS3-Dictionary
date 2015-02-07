f372ddc98ad78040d5af1216de264c36eb37bdf9
Hallie Lomax <rachel.lomax@bison.howard.edu>
2015-02-06 21:22:36 -0500

    uuuuugh

--- cpp/dictClass.h
+++ cpp/dictClass.h
@@ -1,8 +1,9 @@
-#include <strings>
+#include <string>
+#include <iostream>
 
 struct dictEntry {
-    string word;
-    string definition;
+    std::string word;
+    std::string definition;
 };
 
 struct node {
@@ -14,17 +15,77 @@ struct node {
 class dictType {
     private:
 	node* root;
+        void addEntry(dictEntry e, node* leaf);
+
     public:
         dictType();
         void addEntry(dictEntry e);
-	void deleteEntry(string word);
-	void findEntry(string word);
-}
+	void deleteEntry(std::string word);
+	void findEntry(std::string word, node* leaf);
+        void print(node* leaf);
+};
 
 dictType::dictType() {
     node* root = NULL;
 }
 
-void dictType::addEntry() {}
-void dictType::deleteEntry() {}
-void dictType::findEntry() {}
+void dictType::addEntry(dictEntry e) {
+    if (root != NULL) {
+	addEntry(e, root);
+    }
+    else {
+	root = new node;
+        root->info = e;
+        root->left = NULL;
+        root->right = NULL;
+    }
+}
+
+void dictType::addEntry(dictEntry e, node* leaf) {
+    if (e.word < leaf->info.word) {
+        if (leaf->left != NULL) {
+            addEntry(e, leaf->left);
+        }
+        else {
+            leaf->left = new node;
+	    leaf->left->info = e;
+	    leaf->left->left = NULL;
+	    leaf->left->right = NULL;
+	}
+    }
+    else {
+        if (leaf->right != NULL) {
+            addEntry(e, leaf->right);
+	}
+	else {
+	    leaf->right = new node;
+	    leaf->right->info = e;
+	    leaf->right->left = NULL;
+	    leaf->right->right = NULL;
+	}
+    }
+}
+
+void dictType::deleteEntry(std::string word) {}
+
+void dictType::findEntry(std::string word, node* leaf) {
+    if (leaf != NULL) {
+	if (word == leaf->info.word) {
+	    print(leaf);
+	}
+	else if (word < leaf->info.word) {
+	    findEntry(word, leaf->left);
+	}
+	else {
+	    findEntry(word, leaf->right);
+	}
+    }
+    else {
+	std::cout << "Word not found";
+    }
+}
+
+void dictType::print(node* leaf) {
+    std::cout << leaf->info.word << std::endl;
+    std::cout << leaf->info.definition;
+}
